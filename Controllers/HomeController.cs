6dcec04a98add0ae29421b6a50a0bca06afd15fe
Lee Jen Tun <bmpsst511@gmail.com>
2021-06-28 06:03:02 +0800

    home sub pic complete and readme update

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -28,22 +28,10 @@ namespace LMcomposite.Controllers
         public async Task<IActionResult> Index()
         {
             ViewBag.banner = dbContext.Banner;
-            ViewBag.banner1 = dbContext.Banner.Where(x => x.bannerIndex == 1).ToList();
-            ViewBag.banner2 = dbContext.Banner.Where(x => x.bannerIndex == 2).ToList();
-            ViewBag.banner3 = dbContext.Banner.Where(x => x.bannerIndex == 3).ToList();
-            ViewBag.banner4 = dbContext.Banner.Where(x => x.bannerIndex == 4).ToList();
-            ViewBag.banner5 = dbContext.Banner.Where(x => x.bannerIndex == 5).ToList();
             ViewBag.subBoards = dbContext.SubBoard;
-
             var banners = await dbContext.Banner.ToListAsync();
             return View(banners);
         }
-
-        /*public IActionResult Index()
-        {
-            var banners = await dbContext.Banners.ToListAsync();
-            return View();
-        }*/
 
         public IActionResult Privacy()
         {
