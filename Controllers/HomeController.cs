0199661458ddc6db731393035d820dc536353c85
Lee Jen Tun <bmpsst511@gmail.com>
2021-07-02 04:03:57 +0800

    HomeController give db searching and add method

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -28,7 +28,9 @@ namespace LMcomposite.Controllers
         public async Task<IActionResult> Index()
         {
             ViewBag.banner = dbContext.Banner;
-            ViewBag.subBoards = dbContext.SubBoard;
+            ViewBag.midpicture_1 = dbContext.Banner.Where(x => x.bannerIndex > 10 && x.bannerIndex < 15).ToList();//banner 順序設定在這個區間是科技講座的圖
+            ViewBag.midpicture_2 = dbContext.Banner.Where(x => x.bannerIndex > 15 && x.bannerIndex < 20).ToList();//banner 順序設定在這個區間是商品陳列的圖
+            ViewBag.subBoards = dbContext.SubBoard.OrderBy(x => x.Sub_Index).ToList(); //子板照著設定的Index做排列塞入List
             var banners = await dbContext.Banner.ToListAsync();
             return View(banners);
         }
